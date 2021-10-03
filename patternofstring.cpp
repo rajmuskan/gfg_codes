962361d4f4350b674d036f841664158c2d914cdb
Muskan Raj <81063485+rajmuskan@users.noreply.github.com>
2021-10-03 16:16:34 +0530

    Create patternofstring.cpp

---
+++ patternofstring.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	vector<string> pattern(string S)
+	{
+	    // Your code goes here
+	    vector<string> v;
+        for(int i = S.size(); i >= 1; i--)
+        v.push_back(S.substr(0, i));
+        return v;
+	}
+};
+
+// { Driver Code Starts.
+
+int main() 
+{
+   	
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	string tc;
+   	getline(cin, tc);
+   	t = stoi(tc);
+   	while(t--)
+   	{
+   		string s;
+   		getline(cin, s);
+
+   		Solution ob;
+
+   		vector<string> ans = ob.pattern(s);
+
+   		for(int i = 0; i < ans.size(); i++)
+   			cout << ans[i] << "\n";
+   		
+   	}
+
+    return 0;
+}  // } Driver Code Ends
